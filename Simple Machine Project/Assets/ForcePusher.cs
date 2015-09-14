f9a34e9a0ce3d5451ca430a86deff08454c80ce6
Myrto <diakv115@newschool.edu>
2015-09-14 22:40:11 +0300

    step 3

---
+++ Simple Machine Project/Assets/ForcePusher.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForcePusher : MonoBehaviour {
+
+	public float xForce;
+	public int zTourqe;
+
+	private GameObject redCube;
+
+	// Use this for initialization
+	void Start () {
+		xForce = 0;
+		zTourqe = 230;
+		redCube = GameObject.Find ("redCube");
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+		// when I hit the key space, move the object
+		// if I hit the space key
+		// if (this stuff is true) }
+    if ( Input.GetKey( KeyCode.Space ) == true ){
+			rb.AddForce( new Vector3( xForce, 0, 0 ) );
+			rb.AddTorque( new Vector3( 0, 0, 150) );
+			redCube.transform.localScale = new Vector3( 2, 3, 1);
+		
+	}
+		Debug.Log (Input.GetAxis ("Horizontal"));
+		rb.AddForce (new Vector3 (20, 0, 0) * Input.GetAxis ("Horizontal"));
+
+	}
+}
